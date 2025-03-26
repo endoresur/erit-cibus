fb8e89d0aa8f6696dfd5cb06d47d381760a35d3d
endoresur <mlhv2lv@gmail.com>
2025-03-27 01:12:19 +0500

    Added styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,7 @@
+import './styles/fonts.scss'
+import './styles/global.scss'
+import './styles/normalize.css'
+import './styles/root-variables.scss'
 import './App.css'
 
 function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "app": path.resolve(__dirname, "src/app"),
+      "styles": path.resolve(__dirname, "src/styles"),
+      "assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "styles/index.scss" as *;`,
+      },
+    },
+  }
 })
